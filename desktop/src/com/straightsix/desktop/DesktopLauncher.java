708286215810857d7dc93c96650347433b19fca9
james clarke <jkc200013@gmail.com>
2020-01-25 17:32:40 +0000

    added main setup files

---
+++ desktop/src/com/straightsix/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.straightsix.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.straightsix.StraightSix;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StraightSix(), config);
+	}
+}
